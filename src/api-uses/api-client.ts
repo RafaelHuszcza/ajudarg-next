a396afa91d78a7ed93d975d9b9ce9fada866b257
Luan <timpsembs.7@gmail.com>
2024-05-14 15:12:12 -0300

    hope it works

--- src/api-uses/api-client.ts
+++ src/api-uses/api-client.ts
@@ -1,7 +1,12 @@
 import axios from 'axios'
+import { cookies } from 'next/headers'
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+      Cookie: cookies().toString()
+  }
+
 })
 
 export const API_METHODS = {
