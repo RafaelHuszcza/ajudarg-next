0982a447dd668a54819905513f8b01a3097e9bc4
Luan <timpsembs.7@gmail.com>
2024-05-14 14:47:32 -0300

    cors

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.externals = [...config.externals, 'bcrypt']
-    return config
-  },
-}
-
-export default nextConfig
